89eb0f3173114f3d270da62a197c4a48823021a3
mochhilmi <mochhilmi45@gmail.com>
2020-03-29 12:40:51 +0700

    swap

--- Swaping/swap.py
+++ Swaping/swap.py
@@ -1,8 +1,8 @@
-def swap(a,b):
-    c = b
-    b = a
-    a = c
-    return(a,b)
+def swap(teh,kopi):
+    susu = kopi
+    kopi = teh
+    teh = susu
+    return(teh,kopi)
 
 awal = input("masukkan iputan awal : ")
 akhir = input("masukkan inputan akhir : ")
@@ -10,7 +10,7 @@ akhir = input("masukkan inputan akhir : ")
 print ("\ninputan awal : ", awal)
 print ("inputan akhir : ", akhir)
 
-awal, akhir = swap(b=akhir, a=awal)
+awal, akhir = swap(kopi=akhir, teh=awal)
 
 print("\ninputan setelah swap : ")
 print("nilai awal : ",awal, "\nnilai akhir : ",akhir)
